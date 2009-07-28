5ee2cf836475652db101e44338a02f6e6f78b306
Michael.McTernan <Michael.McTernan@2e55c7dc-2cda-11de-8ffe-b512714d229c>
2009-07-28 21:05:23 +0000

    Fix tab indentation.

--- trunk/src/adraw_int.h
+++ trunk/src/adraw_int.h
@@ -47,7 +47,7 @@ Boolean NullInit(struct ADrawTag *outContext);
 Boolean GdoInit(unsigned int     w,
                 unsigned int     h,
                 const char      *file,
-		const char      *fontName,
+                const char      *fontName,
                 struct ADrawTag *outContext);
 
 Boolean PsInit(unsigned int     w,
