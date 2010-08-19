198817714b1462a460ed24286fa836d2dd8db8fa
Michael.McTernan <Michael.McTernan@2e55c7dc-2cda-11de-8ffe-b512714d229c>
2010-08-19 09:55:33 +0000

    Bump .spec file package version to 0.18. Fix typos.

--- trunk/packaging/manual/linux.sh
+++ trunk/packaging/manual/linux.sh
@@ -11,7 +11,7 @@ DIST_VER=`echo "$DIST_FILE" | sed "s/^.*-\([0-9]\+.[0-9]\+\).*$/\1/"`
 
 rm -rf binstage buildstage mscgen-$DIST_VER
 
-# Copy any unpack the source bundle
+# Copy and unpack the source bundle
 cp $DIST_FILE mscgen-src-$DIST_VER.tar.gz
 tar -xzf mscgen-src-$DIST_VER.tar.gz
 
